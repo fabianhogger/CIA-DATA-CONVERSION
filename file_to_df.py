95b6e9cd44536a83a19aec62517ab6a11f8b3370
Fabian <draizfabian@gmail.com>
2021-01-21 19:13:50 +0200

    text files converted to one csv

--- file_to_df.py
+++ file_to_df.py
@@ -1,13 +1,19 @@
 import pandas as pd
 import os
-arr = os.listdir('cia_texts')
+
+"""arr = os.listdir('cia_texts')
 with open('cia.csv', 'a') as the_file:
     for filename  in arr:
         f = open('cia_texts/'+filename,"r")
-        the_file.write(filename+'|'+f.read()+'\n')
+        filename=filename.replace(".txt",'')
+        filetext=f.read()
+        filetext=filetext.replace('|','')
+        print(filename.replace(".txt",''))
+        print(filetext)
+        the_file.write(filename+'|'+filetext+'|')
 
 """
 
 df=pd.read_csv("cia.csv",header=None,sep="|")
+df.columns=["filename","filetext"]
 print(df.head())
-"""
